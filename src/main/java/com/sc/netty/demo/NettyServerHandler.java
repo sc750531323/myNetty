f58230445847f8de39f73bd4a39c6082aa6bb010
sc750531323 <750531323@qq.com>
2022-01-08 00:03:56 +0800

    aaa

--- src/main/java/com/sc/netty/demo/NettyServerHandler.java
+++ src/main/java/com/sc/netty/demo/NettyServerHandler.java
@@ -3,7 +3,6 @@ package com.sc.netty.demo;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
